7d46e9da75e82c28d6a125543dc96889375a9eff
marcelohenrique10 <marcelohenrique.one@gmail.com>
2025-08-13 21:03:23 -0300

    ovo frito 2.0

---
+++ src/funcoes.py
@@ -0,0 +1,25 @@
+passos = [
+    "Pegar 1 ovo",
+    "Aquecer a frigideira",
+    "Adicionar um fio de oleo",
+    "Quebrar o ovo na frigiderira",
+    "Adicionar sal e pimenta",
+    "Adicionar orégano"
+    "Cozinhar por alguns minutos",
+    "Desligar o fogo"
+    "Servir"
+]
+
+"""
+passos =[
+    "passo 1": "pegar o ovo",
+    "passo 2":  "Aquecer a frigideira",
+    "passo 3": "Adicionar um fio de oleo",
+    "passo 4": "Quebrar o ovo na frigiderira",
+    "passo 5": "Cozinhar por alguns minutos",
+    "passo 6": "Servir",
+]
+"""
+
+for i, passo in enumerate(passos, start=1):
+    print(f"{i},{passo}")
